bce115c91958ea438182c7dfa7767676014b417d
cookie <mail@vks.info>
2023-09-18 11:31:45 +0200

    add endboss class

---
+++ levels/level1.js
@@ -0,0 +1,32 @@
+const level1 = new Level(
+    [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+        new Chicken()
+    ],
+    [
+        new Cloud()
+    ],
+    [ 
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/air.png', -1279, 0),
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/3_third_layer/2.png', -1280, 0),
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/2_second_layer/2.png', -1280, 0),
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/1_first_layer/2.png', -1280, 0),
+
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/air.png', 0, 0),
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/3_third_layer/1.png', 0, 0),
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/2_second_layer/1.png', 0, 0),
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/1_first_layer/1.png', 0, 0),
+
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/air.png', 1279, 0),
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/3_third_layer/2.png', 1279, 0),
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/2_second_layer/2.png', 1279, 0),
+        new BackgroundObject('img/img_pollo_locco/5_background/layers/1_first_layer/2.png', 1279, 0),
+    ]
+);
