b970b745ec58d8c1c5ff09c6295d8566e37252f8
cookie <mail@vks.info>
2023-09-18 12:27:26 +0200

    add boss

--- levels/level1.js
+++ levels/level1.js
@@ -8,11 +8,13 @@ const level1 = new Level(
         new Chicken(),
         new Chicken(),
         new Chicken(),
-        new Chicken()
+        new Chicken(),
+        new Endboss()
     ],
     [
         new Cloud()
     ],
+
     [ 
         new BackgroundObject('img/img_pollo_locco/5_background/layers/air.png', -1279, 0),
         new BackgroundObject('img/img_pollo_locco/5_background/layers/3_third_layer/2.png', -1280, 0),
@@ -28,5 +30,5 @@ const level1 = new Level(
         new BackgroundObject('img/img_pollo_locco/5_background/layers/3_third_layer/2.png', 1279, 0),
         new BackgroundObject('img/img_pollo_locco/5_background/layers/2_second_layer/2.png', 1279, 0),
         new BackgroundObject('img/img_pollo_locco/5_background/layers/1_first_layer/2.png', 1279, 0),
-    ]
+    ],
 );
---
+++ models/endboss.class.js
@@ -0,0 +1,30 @@
+class Endboss extends MovableObject {
+    width = 500;
+    height = 500;
+
+    y = 0;
+
+    IMAGES_WALKING = [
+        'img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G5.png',
+        'img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G6.png',
+        'img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G7.png',
+        'img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G8.png',
+        'img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G9.png',
+        'img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G10.png',
+        'img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G11.png',
+        'img/img_pollo_locco/4_enemie_boss_chicken/2_alert/G12.png'
+    ];
+
+    constructor(){
+        super().loadImage(this.IMAGES_WALKING[0]);
+        this.loadImages(this.IMAGES_WALKING);
+        this.x = 700;
+        this.animate();
+    }
+    
+    animate(){
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+         }, 250);
+    }
+}
